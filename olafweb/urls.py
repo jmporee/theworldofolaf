84b9e65469882ab26eb83f8de84c04b593a8c4de
jmporee <jm.poree@laposte.net>
2018-06-20 15:30:46 +0200

    nouvelle sauvegarde

--- olafweb/urls.py
+++ olafweb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from olafweb import views
-
+from django.conf.urls import url
 
 urlpatterns = [
 
     path('', views.home),
+    path('date', views.date_actuelle),
+    path('addition/<int:nombre1>/<int:nombre2>/', views.addition)
 ]
